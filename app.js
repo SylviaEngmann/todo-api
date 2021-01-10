099a151a92b2abf594c31fd53f4ed570ff1067ae
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-01-10 05:48:32 +0000

    Additional routes and queries created

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require("cors");
+const app = express()
 
 var corsOptions = {
   origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
-const app = express()
 const port = 3600
 const db = require('./queries')
 
